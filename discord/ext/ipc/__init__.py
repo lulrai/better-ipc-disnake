d8ff31ada2dc315bffd8fa6232b2b5318f37eac4
Nischal neupane <12538449+lulrai@users.noreply.github.com>
2023-03-07 14:16:28 +0100

    Update __init__.py

--- discord/ext/ipc/__init__.py
+++ discord/ext/ipc/__init__.py
@@ -8,15 +8,15 @@ library designed to work with the latest version of discord.py
 :copyright: (C) 2022 DaPandaOfficial
 :license: GNU GENERAL PUBLIC LICENSE
 """
-import discord
+import disnake
 
-if discord.version_info.major < 2:
-    raise RuntimeError("You must have discord.py (v2.0 or greater) to use this library.")
+if disnake.version_info.major < 2:
+    raise RuntimeError("You must have disnake (v2.0 or greater) to use this library.")
 
-__title__ = "better-ipc"
-__author__ = "DaPandaOfficial"
+__title__ = "better-ipc-disnake"
+__author__ = "Raizusekku"
 __license__ = "GNU GENERAL PUBLIC LICENSE"
-__copyright__ = "Copyright (C) 2022 DaPandaOfficial"
+__copyright__ = "Copyright (C) 2022 Raizusekku"
 __version__ = "2.0.1"
 
 
